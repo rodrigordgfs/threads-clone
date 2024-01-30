261b04b414e3778881d2b946daa5fee00dad2f78
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-01-30 00:49:36 -0300

    Feature: Created the component to create a new post

--- app.vue
+++ app.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
-    teste
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <CreatePost
+      :class="[
+        {
+          'max-h-[100vh] transition-all duration-200 ease-in visible':
+            userStore.isMenuOverlay,
+        },
+        {
+          'max-h-[0px] transition-all duration-200 ease-out invisible':
+            !userStore.isMenuOverlay,
+        },
+      ]"
+    />
   </div>
 </template>
+
+<script setup>
+const userStore = useUserStore();
+</script>
